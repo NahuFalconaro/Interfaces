cc1d46375391a9141cef8314305e89e19bc7c06b
NahuFalconaro <nfalconaro5@gmail.com>
2021-09-03 12:58:21 -0300

    started project

---
+++ EntregablePractico1/js/index.js
@@ -0,0 +1,34 @@
+let canvas = document.getElementById("red");
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF0000BB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("green");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#00FF00BB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("blue");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#0000FFBB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("yellow");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FFFF00BB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("pink");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#FF00FFBB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("lightblue");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#00FFFFBB";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+canvas = document.getElementById("black");
+ctx = canvas.getContext("2d");
+ctx.fillStyle = "#000000FF";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
