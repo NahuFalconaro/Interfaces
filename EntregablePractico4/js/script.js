8e4df556dce5c7ecba81387dbd58bb247f8bfa88
nahufalconaro <nfalconaro5@gmail.com>
2021-11-17 17:09:06 -0300

    Creado de archivos para particulas

--- EntregablePractico4/js/script.js
+++
@@ -1,5 +0,0 @@
-// https://www.youtube.com/watch?v=lTeS8Xv3ymA&ab_channel=FalconMasters
-// https://vincentgarreau.com/particles.js/
-
-// ANotaciones: Desde esa libreria se obtiene el codigo para modificar las particulas, en este proyecto se
-// uso un cdn de un tutorial
